b954dfb0ef3fca7b387256cd46358f00d0045ce2
Frank Christiaan Peelen <frank.peelen@yahoo.com>
2015-05-01 15:05:55 +0900

    Fix password representation in database

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'faker', '1.4.2'
+gem 'bcrypt', '~> 3.1.7'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.1'
--- db/migrate/20150501055857_add_password_to_user.rb
+++ db/migrate/20150501055857_add_password_to_user.rb
@@ -1,6 +1,5 @@
 class AddPasswordToUser < ActiveRecord::Migration
   def change
-    add_column :users, :password, :string
-    add_column :users, :password_confirmation, :string
+    add_column :users, :password_digest, :string
   end
 end
